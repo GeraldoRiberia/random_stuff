c33dbf28d50e07cca8ac69efc99bc3d4bf7b1b97
GeraldoRiberia <91908767+GeraldoRiberia@users.noreply.github.com>
2022-08-25 16:58:24 +0530

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,3 +1,6 @@
-import pywhatkit as ass
-for i in range(5) :
-    ass.sendwhatmsg("+91 94951 73307","Nice to meet you",21,13)
+x = 1123
+print(x)
+if x<1234 :
+    print("Balls")
+else :
+    print("Big Balls")
